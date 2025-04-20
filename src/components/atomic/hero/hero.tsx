c5756310aad75992e26152500fc80e2b5c056c48
Ryendel Rocha <35112351+RyendelRocha@users.noreply.github.com>
2025-04-19 22:29:39 -0300

    fix: text adjusts

--- src/components/atomic/hero/hero.tsx
+++ src/components/atomic/hero/hero.tsx
@@ -7,9 +7,9 @@ interface HeroProps {
 
 export const Hero: React.FC<HeroProps> = ({ title, description }) => {
   return (
-    <div className="mx-auto flex h-[40vh] max-w-screen-xl flex-col items-center justify-center py-12 text-center">
-      <h1 className="mb-4 text-4xl font-bold text-gray-800 md:text-[80px]">{title}</h1>
-      <p className="textSubtle max-w-xl text-2xl">{description}</p>
+    <div className="mx-auto flex h-[40vh] max-w-screen-lg flex-col items-center justify-center py-8 text-center md:py-12">
+      <h1 className="displayPrimary mb-4 text-gray-800 ">{title}</h1>
+      <p className="textSubtle max-w-xl text-xl md:text-2xl">{description}</p>
     </div>
   );
 };
